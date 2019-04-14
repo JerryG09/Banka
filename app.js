8cd0b7bfa2dcfc41783df094150920ce4caff74c
Oyebamiji Jeremiah <phemmy09@gmail.com>
2019-04-14 17:08:36 +0100

    ft(signup endpoint):

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import route from './route/index';
+import route from './route/index';
 
-const PORT = parseInt(process.env.PORT, 10) || 4000;
+const PORT = parseInt(process.env.PORT, 10) || 4040;
 
 const app = express();
 
@@ -10,13 +10,12 @@ app.get('/', (req, res) => res.status(200).json({
   messsag: 'app home page'
 }))
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
-}))
+}));
 
-// app.use(route)
-// app.use('/api', router);
+app.use(route);
+
 
 app.route('/*').all((req, res) => res.status(404).json({
   status: 404,
