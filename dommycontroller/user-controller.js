88d79be6d3d3de51f510cc3e4b2ebef5b7f15ec0
Oyebamiji Jeremiah <phemmy09@gmail.com>
2019-04-14 19:25:48 +0100

    ft(signin endpoit)

--- dommycontroller/user-controller.js
+++ dommycontroller/user-controller.js
@@ -3,78 +3,124 @@ import jwt from 'jsonwebtoken';
 import bcryt from 'bcryptjs';
 import config from 'config';
 
-class User{
+class User {
   /**
-  * @param {*} req 
-  * @param {*} res
-  * @returns {*} json 
-  */
- static createUser (req, res){
-   const user = {
-     id: db.length + 1,
-     firstName: req.body.firstName,
-     lastName: req.body.lastName,
-     email: req.body.email,
-     type: req.body.type,
-     isAdmin: req.body.isAdmin,
-     password: req.body.password
-   }
-
-   if (!user.firstName || !user.lastName || !user.email || !user.type || !user.isAdmin || !user.password) {
-     return res.status(400).json({ msg: 'Please enter all fields'})
-   }
-
-  //  Check for existing user
-  db.map((data)=> {
-    if (data.email === user.email) {
-      return res.status(400).json({ msg: 'User already exits' })
-    }
-  })
-
-  // Create sat and hash
-  bcryt.genSalt(10, (err, salt) => {
-    bcryt.hash(user.password, salt, (err, hash) => {
-      if (err) throw err;
-      user.password = hash;
-      db.push(user);
-
-      // Sign Token
-      jwt.sign(
-        { isAdmin: user.isAdmin, type: user.type},
-        config.get('jwtSecret'),
-        { expiresIn: 3600 },
-        (err, token) => {
-          if(err) throw err;
-          return res.status(201).json({
-            status: 201,
-            token,
-            user: {
-              id: user.id,
-              firstName: user.firstName,
-              lastName: user.lastName,
-              email: user.email
-            }
-        }
-      )
-   })
-    })
-  })
- }
-
- /**
    * @param {*} req 
    * @param {*} res
    * @returns {*} json 
    */
-  static getAllUsers(req, res) {
-    return res.status(200).json({
-      status: 200,
-      message: "All user accounts retrieved successfully",
-      data: db
+  static createUser(req, res) {
+    const user = {
+      id: db.length + 1,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      type: req.body.type,
+      isAdmin: req.body.isAdmin,
+      password: req.body.password
+    }
+
+    if (!user.firstName || !user.lastName || !user.email || !user.type || !user.isAdmin || !user.password) {
+      return res.status(400).json({
+        msg: 'Please enter all fields'
+      })
+    }
+
+    //  Check for existing user
+    db.map((data) => {
+      if (data.email === user.email) {
+        return res.status(400).json({
+          msg: 'User already exits'
+        })
+      }
+    })
+
+    // Create salt and hash
+    bcryt.genSalt(10, (err, salt) => {
+      bcryt.hash(user.password, salt, (err, hash) => {
+        if (err) throw err;
+        user.password = hash;
+        db.push(user);
+
+        // Sign Token
+        jwt.sign({
+            isAdmin: user.isAdmin,
+            type: user.type
+          },
+          config.get('jwtSecret'), {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) throw err;
+            return res.status(201).json({
+              status: 201,
+              token,
+              user: {
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email
+              }
+            })
+          })
+      })
     })
   }
 
- 
+  /**
+   * @param {*} req 
+   * @param {*} res
+   * @returns {*} json 
+   */
+  static loginUser(req, res) {
+    if (!user.email || !user.password) {
+      return res.status(400).json({
+        msg: 'Please enter all fields'
+      })
+    }
+
+    //  Check for user
+    db.map((data) => {
+      if (!data.email === user.email) {
+        return res.status(400).json({
+          msg: 'User does not exit'
+        })
+
+        // Validate password
+        bcrypt.compare(password, data.password)
+          .then(isMatch => {
+            if (!isMatch) return res.status(400).json({
+              msg: 'Invalid credentials'
+            });
+
+            // Sign Token
+            jwt.sign({
+                isAdmin: user.isAdmin,
+                type: user.type
+              },
+              config.get('jwtSecret'), {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                if (err) throw err;
+                return res.status(201).json({
+                  status: 201,
+                  token,
+                  user: {
+                    id: user.id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email
+                  }
+                })
+              })
+
+          })
+      }
+    })
+  }
+
+
 
 }
 
